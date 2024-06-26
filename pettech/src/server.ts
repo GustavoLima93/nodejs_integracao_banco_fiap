e70930ce33272c75e6e8b4abbfd695d8fa1a0cea
oc-garcia <octaviogarcia1988@gmail.com>
2024-06-25 23:23:16 -0300

    Update server.ts to use environment variables for host and port configuration

--- pettech/src/server.ts
+++ pettech/src/server.ts
@@ -1,11 +1,9 @@
-import { env } from './env'
 import { app } from '@/app'
+import { env } from '@/env'
+
+const HOST = env.DATABASE_HOST || '0.0.0.0'
+const PORT = env.PORT || 3000
 
 app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log(`Server is running on http://localhost:${env.PORT}`)
-  })
+  .listen({ host: HOST, port: PORT })
+  .then(() => console.log(`Server is running on http://${HOST}:${PORT}`))
